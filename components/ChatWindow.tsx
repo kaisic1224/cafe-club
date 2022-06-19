500ea41cfe290482c769c44cbef7b5d142d5053c
Christopher Fong <79167402+christopherfong115@users.noreply.github.com>
2022-06-19 02:40:35 -0400

    Add files via upload

--- components/ChatWindow.tsx
+++ components/ChatWindow.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { user } from "./FriendsModal";
 import { FaPaperPlane } from "react-icons/fa";
+import { ChatBubble } from "./ChatBubble";
 
 const socket = io("http://localhost:3001");
 
@@ -17,7 +18,7 @@ const ChatWindow = () => {
   function sendMessage() {
     socket.emit("msg", {
       content: send,
-      sender: "me"
+      sender: "me",
     });
   }
 
@@ -28,17 +29,52 @@ const ChatWindow = () => {
   }, [messages]);
 
   return (
-    <div className='absolute top-0 right-0 min-h-screen bg-amber-900'>
-      <ul className='flex flex-col overflow-y-scroll'>
-        {messages.map((msg) => (
-          <li>{msg.content}</li>
-        ))}
-      </ul>
-      <div>
-        <input type='text' />
-        <label htmlFor=''></label>
+    <>
+      <div className="absolute top-0 right-0 h-fit bg-amber-900 max-w-md w-md">
+        <div className="flex flex-col overflow-y-auto max-w-md w-md">
+          <div className="flex p-4 w-fit max-w-md">
+            <div className="w-fit text-white rounded-2xl break-word">
+              <span className="">
+                Please be respectful in chat! Refrain from using profanity and
+                enjoy :)
+              </span>
+            </div>
+          </div>
+          {messages.map((msg) => (
+            <ChatBubble msg={msg} />
+          ))}
+        </div>
       </div>
-    </div>
+      <a
+        className="bg-orange-400 px-4 py-2 text-white rounded fixed bottom-0"
+        href="/"
+      >
+        Go back
+      </a>
+      <div className="relative">
+        <div className="min-w-2xl fixed bottom-0 right-[425px]">
+          <input
+            value={send}
+            className="min-h-[40px] rounded-md bg-rose-100 w-3/5 ml-6 p-2 outline-none font-bold"
+            placeholder="Type here..."
+            onChange={(e) => {
+              setSend(e.target.value);
+            }}
+            type="text"
+          />
+          <label htmlFor=""></label>
+          <button
+            className="text-white px-4 py-2 rounded bg-orange-300 ml-2"
+            onClick={() => {
+              sendMessage();
+              setSend("");
+            }}
+          >
+            Send
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 export default ChatWindow;
