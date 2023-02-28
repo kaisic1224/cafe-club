a275e688dbad341b3c94cff45242cb6b1039cabe
kaisic1224 <hiddenshadows1224@gmail.com>
2023-02-27 20:12:49 -0500

    stash for now

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "../../../lib/prisma";
 
 export default NextAuth({
   // Configure one or more authentication providers
-  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.CLIENT_ID!,
@@ -14,13 +12,13 @@ export default NextAuth({
         params: {
           prompt: "consent",
           access_type: "offline",
-          response_type: "code",
-        },
-      },
-    }),
+          response_type: "code"
+        }
+      }
+    })
   ],
   secret: process.env.NEXTAUTH_SECRET!,
   pages: {
-    signIn: "/signin",
-  },
+    signIn: "/signin"
+  }
 });
